03be28c39a8ce9ee7632bffa6b4b1b889a6e3c70
Juan-Code-Gomez <jgomezberrio054@gmail.com>
2024-06-17 19:34:35 -0500

    Se quitan mas comillas

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -12,9 +12,9 @@ const App = () => {
     const handleRouteChange = (url: string) => {
       gtag.pageview(url);
     };
-    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on('routeChangeComplete', handleRouteChange);
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off('routeChangeComplete', handleRouteChange);
     };
   }, [router.events]);
 
@@ -22,12 +22,12 @@ const App = () => {
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
-        strategy="afterInteractive"
+        strategy='afterInteractive'
         src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
-        id="gtag-init"
-        strategy="afterInteractive"
+        id='gtag-init'
+        strategy='afterInteractive'
         dangerouslySetInnerHTML={{
           __html: `
             window.dataLayer = window.dataLayer || [];
@@ -39,7 +39,7 @@ const App = () => {
           `,
         }}
       />
-      <Script async src={GA_ADS_ID} crossOrigin="anonymous" />
+      <Script async src={GA_ADS_ID} crossOrigin='anonymous' />
     </>
   );
 };
